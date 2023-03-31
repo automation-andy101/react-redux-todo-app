c511b930dc819c4b04bb23de98881e3f2c0264cb
Andy Short <automation.andy101@gmail.com>
2023-03-31 06:53:33 +0100

    first commit

---
+++ client/src/components/TotalCompleteItems.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+const TotalCompleteItems = () => {
+  // return all todos that are marked as completed (true)
+  const completedTodos = useSelector((state) => 
+    state.todos.filter((todo) => todo.completed === true)
+  );
+
+  return (
+    <h4 className='mt-3'> 
+      Total Complete Items: {completedTodos.length}
+    </h4>
+  )
+}
+
+export default TotalCompleteItems;
